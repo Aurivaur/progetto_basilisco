8f2173fbcaf2631c5cad50de3cfffc8af4698470
Aurora <aurivaur@gmail.com>
2024-04-16 17:50:05 +0200

    sistemato elimina areaadmin

--- src/app/areaamministratore/tabellaprodotti/tabellaprodotti.component.ts
+++ src/app/areaamministratore/tabellaprodotti/tabellaprodotti.component.ts
@@ -1,11 +1,9 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Disco } from 'src/models/Disco';
 import { Gioco } from 'src/models/Gioco';
 import { Libro } from 'src/models/Libro';
-import { Persona } from 'src/models/Persona';
-import { Prodotto } from 'src/models/Prodotto';
 import { Utente } from 'src/models/Utente';
 
 @Component({
@@ -40,6 +38,88 @@ export class TabellaprodottiComponent {
   
   toggleVediDischi(){
     this.isDisco = !this.isDisco;
+  }
+
+  //ELIMINAZIONE VARI PRODOTTI
+  deleteDischi(id : number){
+    let token = sessionStorage.getItem("token");
+    if(token == null){
+      token = "";
+    }
+
+    const headers = new HttpHeaders(
+      {
+        'Content-Type' : 'application/json',
+        'token': token as string
+      }
+    );
+
+    const params = new HttpParams().set('idAdmin', id);
+
+    this.http.get<boolean>("http://localhost:8080/api/admin/delete", {headers, params}).subscribe(risposta =>{
+      if(risposta){
+        alert("Eliminazione avvenuta con successo");
+        let pos = this.dischi?.findIndex(x => x.id === id)
+        if(pos! > -1){
+          this.dischi?.splice(pos!, 1);
+        }
+      }
+    })
+
+  }
+
+  deleteLibri(id : number){
+    let token = sessionStorage.getItem("token");
+    if(token == null){
+      token = "";
+    }
+
+    const headers = new HttpHeaders(
+      {
+        'Content-Type' : 'application/json',
+        'token': token as string
+      }
+    );
+
+    const params = new HttpParams().set('idAdmin', id);
+
+    this.http.get<boolean>("http://localhost:8080/api/admin/delete", {headers, params}).subscribe(risposta =>{
+      if(risposta){
+        alert("Eliminazione avvenuta con successo");
+        let pos = this.libri?.findIndex(x => x.id === id)
+        if(pos! > -1){
+          this.libri?.splice(pos!, 1);
+        }
+      }
+    })
+
+  }
+
+  deleteGiochi(id : number){
+    let token = sessionStorage.getItem("token");
+    if(token == null){
+      token = "";
+    }
+
+    const headers = new HttpHeaders(
+      {
+        'Content-Type' : 'application/json',
+        'token': token as string
+      }
+    );
+
+    const params = new HttpParams().set('idAdmin', id);
+
+    this.http.get<boolean>("http://localhost:8080/api/admin/delete", {headers, params}).subscribe(risposta =>{
+      if(risposta){
+        alert("Eliminazione avvenuta con successo");
+        let pos = this.giochi?.findIndex(x => x.id === id)
+        if(pos! > -1){
+          this.giochi?.splice(pos!, 1);
+        }
+      }
+    })
+
   }
 
 
--- src/app/areaamministratore/tabellautenti/tabellautenti.component.ts
+++ src/app/areaamministratore/tabellautenti/tabellautenti.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Utente } from 'src/models/Utente';
 
 @Component({
@@ -12,8 +14,41 @@ export class TabellautentiComponent {
 
   isUtente = false;
 
+  constructor(private http : HttpClient, private formBuider : FormBuilder){
+    this.http = http;
+  }
+
+
   toggleVediUtenti(){
     this.isUtente = !this.isUtente;
+  }
+
+  //CANCELLA UTENTI
+  deleteUtenti(id : number){
+    let token = sessionStorage.getItem("token");
+    if(token == null){
+      token = "";
+    }
+
+    const headers = new HttpHeaders(
+      {
+        'Content-Type' : 'application/json',
+        'token': token as string
+      }
+    );
+
+    const params = new HttpParams().set('idAdmin', id);
+
+    this.http.get<boolean>("http://localhost:8080/api/admin/delete", {headers, params}).subscribe(risposta =>{
+      if(risposta){
+        alert("Eliminazione avvenuta con successo");
+        let pos = this.utenti?.findIndex(x => x.id === id)
+        if(pos! > -1){
+          this.utenti?.splice(pos!, 1);
+        }
+      }
+    })
+
   }
 
 }
