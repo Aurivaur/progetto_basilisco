a7595cea51a5c322cc39218b99a15800c382f3b6
Cappa <federico.cappalunga@gmail.com>
2024-04-19 15:54:25 +0200

    carrello

--- src/app/areautente/carrello/carrello.component.ts
+++ src/app/areautente/carrello/carrello.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Prodotto } from 'src/models/Prodotto';
 
@@ -10,9 +10,9 @@ import { Prodotto } from 'src/models/Prodotto';
 export class CarrelloComponent {
 
   //avrò una lista di prodotti che aggiungo da vari componenti
-  @Input() carrello? : Map<Prodotto, number>;
+  @Input() carrello? : Prodotto[];
 
-  listacarrello! : Prodotto[];
+  //listacarrello! : Prodotto[];
 
   //carrello! : Prodotto[];
 
@@ -35,18 +35,66 @@ export class CarrelloComponent {
       }
     )
 
-    this.http.get<Map<Prodotto,number>>("http://localhost:8080/api/areautente/carrello/all", {headers}).subscribe(risposta =>{
+    this.http.get<Prodotto[]>("http://localhost:8080/api/areautente/carrello/all", {headers}).subscribe(risposta =>{
       this.carrello = risposta;
       console.log(this.carrello);
-      this.listacarrello = Array.from(this.carrello.keys());
+      //this.listacarrello = Array.from(this.carrello.keys());
     })
 
+  }
+
+  elimina(id : number) {
+    let token = sessionStorage.getItem("token");
+    if(token == null){
+      token = "";
+    }
+
+    const headers = new HttpHeaders(
+      {
+        'Content-Type' : 'application/json',
+        'token': token as string
+      }
+    );
+
+    const params = new HttpParams().set('idProdotto', id);
+
+    this.http.get<boolean>("http://localhost:8080/api/areautente/carrello/deleteprodotto", {headers, params}).subscribe(risposta =>{
+      if(risposta){
+        alert("Eliminazione avvenuta con successo");
+        let pos = this.carrello?.findIndex(x => x.id === id)
+        if(pos! > -1){
+          this.carrello?.splice(pos!, 1);
+        }
+      }
+    })
   }
 
 
   acquista() {
     alert("Grazie per l'acquisto!");
+    this.svuota();
     window.location.href="/areautente";
+  }
+
+  svuota() {
+    let token = sessionStorage.getItem("token");
+    if(token == null){
+      token = "";
+    }
+
+    const headers = new HttpHeaders(
+      {
+        'Content-Type' : 'application/json',
+        'token': token as string
+      }
+    );
+
+    //const params = new HttpParams().set('idProdotto', id);
+
+    this.http.get<boolean>("http://localhost:8080/api/areautente/carrello/svuota", {headers}).subscribe(risposta =>{
+      //this.carrello = [];
+      console.log(risposta);
+    })
   }
 
 }
